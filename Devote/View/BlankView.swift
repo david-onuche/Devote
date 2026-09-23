8612c6f1e1724510f52b07be0d8a7c20ea9a60cb
davidonuche <iamdavidonuche@gmail.com>
2026-09-23 21:57:15 +0100

    Update on Balnk View and Content View.

--- Devote/View/BlankView.swift
+++ Devote/View/BlankView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct BlankView: View {
     // MARK: - PROPERTY
-    
+    var backgroundColor: Color
+    var backgroundOpacity: Double
     
     // MARK: - BODY
     var body: some View {
@@ -17,14 +18,17 @@ struct BlankView: View {
             Spacer()
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-        .background(Color.black)
-        .opacity(0.5)
+        .background(backgroundColor)
+        .opacity(backgroundOpacity)
+        .blendMode(.overlay)
         .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct BlankView_Previews: PreviewProvider {
     static var previews: some View {
-        BlankView()
+        BlankView(backgroundColor: .black, backgroundOpacity: 0.3)
+            .background(BackgroundImageView())
+            .background(backgroundGradient.ignoresSafeArea(.all))
     }
 }
--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -132,10 +132,17 @@ struct ContentView: View {
                     .frame(maxWidth: 640)
                     .scrollContentBackground(.hidden)
                 } //: VSTACK
+                .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
+                .transition(.move(edge: .bottom))
+                .animation(.easeOut(duration: 0.5))
+                
                 
                 // MARK: - NEW TASK ITEM
                 if showNewTaskItem {
-                    BlankView()
+                    BlankView(
+                        backgroundColor: isDarkMode ? Color.black : Color.gray,
+                        backgroundOpacity: isDarkMode ? 0.3 : 0.5
+                    )
                         .onTapGesture {
                             withAnimation() {
                                 showNewTaskItem = false
@@ -151,6 +158,7 @@ struct ContentView: View {
             .navigationBarHidden(true)
             .background(
                 BackgroundImageView()
+                    .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
             )
             .background(backgroundGradient.ignoresSafeArea(.all))
             } //: NAVIGATION
