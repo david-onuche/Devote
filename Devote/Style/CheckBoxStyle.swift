962680d0cdcb0e25793985a8f5c6b75b4a583e26
davidonuche <iamdavidonuche@gmail.com>
2026-09-23 21:37:58 +0100

    Individual task item view.

---
+++ Devote/Style/CheckBoxStyle.swift
@@ -0,0 +1,18 @@
+//
+//  CheckBoxStyle.swift
+//  Devote
+//
+//  Created by David Onuche on 23/09/2026.
+//
+
+import SwiftUI
+
+struct CheckBoxStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckBoxStyle()
+}
---
+++ Devote/View/ListRowItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListRowItemView.swift
+//  Devote
+//
+//  Created by David Onuche on 23/09/2026.
+//
+
+import SwiftUI
+
+struct ListRowItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRowItemView()
+}
