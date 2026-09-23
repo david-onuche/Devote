4a35a26291aa21f47e0d5494e527508614a244f0
davidonuche <iamdavidonuche@gmail.com>
2026-09-23 21:38:12 +0100

    Individual task item view.

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -6,7 +6,8 @@
 //
 
 import SwiftUI
-import CoreData
+public import CoreData
+//import CoreData
 
 struct ContentView: View {
     // MARK: - PROPERTY
@@ -121,15 +122,7 @@ struct ContentView: View {
                     
                     List {
                         ForEach(items) { item in
-                            VStack(alignment: .leading) {
-                                Text(item.task ?? "")
-                                    .font(.headline)
-                                    .fontWeight(.bold)
-                                
-                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                                    .font(.footnote)
-                                    .foregroundColor(.gray)
-                            } //: LIST ITEM
+                            ListRowItemView(item: item)
                         }
                         .onDelete(perform: deleteItems)
                     } //: LIST
--- Devote/View/ListRowItemView.swift
+++ Devote/View/ListRowItemView.swift
@@ -6,13 +6,27 @@
 //
 
 import SwiftUI
+internal import CoreData
 
 struct ListRowItemView: View {
+    @Environment(\.managedObjectContext) var viewContext
+    @ObservedObject var item: Item
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Toggle(isOn: $item.completion) {
+            Text(item.task ?? "")
+                .font(.system(.title2, design: .rounded))
+                .fontWeight(.heavy)
+                .foregroundColor(item.completion ? Color.pink : Color.primary)
+                .padding(.vertical, 12)
+                .animation(.default, value: item.completion)
+        } //: TOGGLE
+        .toggleStyle(CheckBoxStyle())
+        .onReceive(item.objectWillChange, perform: { _ in
+            if self.viewContext.hasChanges {
+                try? self.viewContext.save()
+            }
+        })
     }
-}
-
-#Preview {
-    ListRowItemView()
 }
