425cce85efa502a0f05ca6451a59ef6e95cf9eee
davidonuche <iamdavidonuche@gmail.com>
2026-09-23 23:10:54 +0100

    Done with app

--- DevoteWidget/DevoteWidget.swift
+++ DevoteWidget/DevoteWidget.swift
@@ -43,15 +43,57 @@ struct SimpleEntry: TimelineEntry {
 
 struct DevoteWidgetEntryView : View {
     var entry: Provider.Entry
+    @Environment(\.widgetFamily) var widgetFamily
+    
+    var fontStyle: Font {
+        if widgetFamily == .systemSmall {
+            return .system(.footnote, design: .rounded)
+        } else {
+            return .system(.headline, design: .rounded)
+        }
+    }
 
     var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Favorite Emoji:")
-            Text(entry.configuration.favoriteEmoji)
-        }
+        GeometryReader { geometry in
+            ZStack {
+                backgroundGradient
+                
+                Image("rocket-small")
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("logo")
+                    .resizable()
+                    .frame(
+                        width: widgetFamily != .systemSmall ? 56 : 36,
+                        height: widgetFamily != .systemSmall ? 56 : 36)
+                    .offset(
+                        x: geometry.size.width / 2 - 20,
+                        y: (geometry.size.height / -2) + 20
+                    )
+                    .padding(.top, widgetFamily != .systemSmall ? 32 : 12)
+                    .padding(.trailing, widgetFamily != .systemSmall ? 32 : 12)
+                HStack {
+                    Text("David Onuche")
+                        .foregroundStyle(.white)
+                        .font(fontStyle)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 4)
+                        .background(
+                            Rectangle()
+                                .fill(Color(red: 0, green: 0, blue: 0, opacity: 0.5))
+                                .blendMode(.overlay)
+                        )
+                        .clipShape(Capsule())
+                    if widgetFamily != .systemSmall {
+                        Spacer()
+                    }
+                } //: HSTACK
+                .padding()
+                .offset(y: (geometry.size.height / 2) - 24)
+            } //: ZSTACK
+        } //: GEOMETRY
     }
 }
 
@@ -63,6 +105,8 @@ struct DevoteWidget: Widget {
             DevoteWidgetEntryView(entry: entry)
                 .containerBackground(.fill.tertiary, for: .widget)
         }
+        .configurationDisplayName("Devote Lanucher")
+        .description("This is an example widget for the personal task manager app.")
     }
 }
 
@@ -81,6 +125,20 @@ extension ConfigurationAppIntent {
 }
 
 #Preview(as: .systemSmall) {
+    DevoteWidget()
+} timeline: {
+    SimpleEntry(date: .now, configuration: .smiley)
+    SimpleEntry(date: .now, configuration: .starEyes)
+}
+
+#Preview(as: .systemMedium) {
+    DevoteWidget()
+} timeline: {
+    SimpleEntry(date: .now, configuration: .smiley)
+    SimpleEntry(date: .now, configuration: .starEyes)
+}
+
+#Preview(as: .systemLarge) {
     DevoteWidget()
 } timeline: {
     SimpleEntry(date: .now, configuration: .smiley)
