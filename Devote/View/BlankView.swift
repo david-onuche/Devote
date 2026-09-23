b9552c21c397541e0c69c79f7a82044bf72209e8
davidonuche <iamdavidonuche@gmail.com>
2026-09-23 20:44:41 +0100

    New entry form for the task item.

---
+++ Devote/View/BlankView.swift
@@ -0,0 +1,18 @@
+//
+//  BlankView.swift
+//  Devote
+//
+//  Created by David Onuche on 23/09/2026.
+//
+
+import SwiftUI
+
+struct BlankView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlankView()
+}
---
+++ Devote/View/NewTaskItemView.swift
@@ -0,0 +1,18 @@
+//
+//  NewTaskItemView.swift
+//  Devote
+//
+//  Created by David Onuche on 23/09/2026.
+//
+
+import SwiftUI
+
+struct NewTaskItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewTaskItemView()
+}
